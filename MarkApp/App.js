61872aa694ac1bb201d1822cead43b5b634ddc30
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-07-10 17:31:17 +0000

    update and addition of extra files

--- MarkApp/App.js
+++ MarkApp/App.js
@@ -1,19 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-//import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './screens/HomeScreen';
 import CheckoutScreen from './screens/CheckoutScreen';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { CartProvider } from './context/CartContext';
+import ProductDetailScreen from './screens/ProductDetailScreen';
 
-//const Tab = createBottomTabNavigator();
+
 const Stack = createNativeStackNavigator();
 
-// Drawer Navigation
-//const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
@@ -40,22 +37,16 @@ const HomeStack = () => {
         component={CheckoutScreen}
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name="ProductDetailScreen"
+        component={ProductDetailScreen}
+        options={{ headerShown: false }}
+      />
       
     </Stack.Navigator>
   )
 }
 
-// const HomeStack = () => {
-//   return (
-//     <Drawer.Navigator initialRouteName='Home'>
-//       <Drawer.Screen
-//         name="Home"
-//         component={HomeScreen}
-//       />
-      
-//     </Drawer.Navigator>
-//   )
-// }
 
 
 const styles = StyleSheet.create({
